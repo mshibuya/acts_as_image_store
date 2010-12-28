ffd84fb449e412258915a53c74816dab30b8400b
Mitsuhiro Shibuya <shibuya@lavan7.co.jp>
2010-12-28 16:51:16 +0900

    mount_atの設定がない場合はrouteを無効に

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  mount_at = MogileImageStore::Engine.config.mount_at rescue '/image/'
+  begin
+    mount_at = MogileImageStore::Engine.config.mount_at
 
-  match "#{mount_at}:size/:name.:format", :to => "mogile_images#show", :constraints => {
-    :size => /(raw|\d*x\d*)/,
-    :name =>/[0-9a-f]{0,32}/,
-    :format =>/(jpg|gif|png)/,
-  }
+    match "#{mount_at}:size/:name.:format", :to => "mogile_images#show", :constraints => {
+      :size => /(raw|\d*x\d*)/,
+      :name =>/[0-9a-f]{0,32}/,
+      :format =>/(jpg|gif|png)/,
+    }
+  rescue NoMethodError
+    #do nothing
+  end
 end
